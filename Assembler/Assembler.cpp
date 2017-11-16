f8228d3b25a41df12300d70e52987909a6e67d17
jpaz <jriemersma@dundee.ac.uk>
2017-11-16 17:32:13 +0000

    Assember extracts usefull data to be turn into machinecode

--- Assembler/Assembler.cpp
+++ Assembler/Assembler.cpp
@@ -1,63 +1,14 @@
 #include <string>
-#include <vector> 
+#include <vector>
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
 
 using namespace std;
 
-void readAndVectorise()
-{
-
-	string fileName = "";
-	bool exists = false;
-
-	while (fileName == "" && exists == false)
-	{
-
-		cout << "Please enter the name of the file you would like to read from: " << '\n';
-
-		cin >> fileName;
-
-		ifstream myfile(fileName);
-
-		if (myfile.is_open())
-		{
-			cout << "The file you entered exists." << endl;
-
-
-			
-
-
-
-
-
-
-			
-		}
-		else
-		{
-			cin.ignore(100);
-			cin.clear();
-			cout << "The file you entered does not exist" << endl;
-		} 
-	}
-
-	//stringstream ss
-
-	//vector methods, how to read lines of text into vectors 
-
-}
-
-void deleteComments()
-{
-	/*1st pass thru tokens
-
-	get rid of comments from the vector created before
-
-	use .erase to get rid of anything after semi-colon*/
-
-}
-
+vector< vector<string>> toAssemble;
 
 void createSymbolTable()
 {
@@ -72,54 +23,30 @@ void createSymbolTable()
 
 }
 
-void instructionConversion()
-{
+void instructionConversion(){
 	string opCode = ""; //this needs to be passed from symbol table function
 
-	if(opCode == "JMP")
-		{
-			opCode = "000";
-		}
-
-	else if(opCode == "JRP")
-		{
-			opCode = "100";
-		}
-
-	else if(opCode == "LDN")
-		{
-			opCode = "010";
-		}
-
-	else if(opCode == "STO")
-		{
-			opCode = "110";
-		}
-
-	else if(opCode == "SUB")
-		{
-			opCode = "001";
-		}
-
-	else if(opCode == "SUB")
-		{
-			opCode = "101";
-		}
-
-	else if(opCode == "CMP")
-		{
-			opCode = "011";
-		}
-
-	else if(opCode == "STP")
-		{
-			opCode = "111";
-		}
+	if(opCode == "JMP"){
+		opCode = "000";
+	}else if(opCode == "JRP"){
+		opCode = "100";
+	}else if(opCode == "LDN"){
+		opCode = "010";
+	}else if(opCode == "STO"){
+		opCode = "110";
+	}else if(opCode == "SUB"){
+		opCode = "001";
+	}else if(opCode == "SUB"){
+		opCode = "101";
+	}else if(opCode == "CMP"){
+		opCode = "011";
+	}else if(opCode == "STP"){
+		opCode = "111";
+	}
 
 }
 
-void convertToMachineCode()
-{
+void convertToMachineCode(){
 /*
 	address location = operand
 
@@ -129,9 +56,74 @@ void convertToMachineCode()
 
 }
 
-int main () 
 
-{
-	readAndVectorise();
+void CleanLine(vector<string> tokens){ //Removes comments from code;
+	int i = 0;
+	bool clean = false;
+	vector<string> cleaned;
+
+	while(!clean && i < tokens.size()){
+		if(tokens[i].at(0) == ';'){
+			clean = true;
+		}else{
+			cleaned.push_back(tokens[i]);
+		}
+		i++;
+	}
+
+	if(cleaned.size() > 0){
+		toAssemble.push_back(cleaned);
+
+		for(int i=0;i < cleaned.size();i++){
+			cout << cleaned[i] << " ";
+		}
+		cout << endl;
+	}
+
+	return cleaned;
+}
+
+
+vector<string> RemoveWhitespace(string line){ //I will give you a crisp highfive if you can guess what this does...
+
+	//Answer modified from https://stackoverflow.com/questions/19887232/how-to-loop-through-a-string-by-space-how-do-i-know-the-index-no-of-the-word-i
+	//User: deepmax
+	string toAdd;
+	vector<string> tokens;
+
+	for(stringstream s(line); s >> toAdd;){
+	    tokens.push_back(toAdd);
+	}
+	return tokens;
+}
+
+bool ReadFile(string fileName){ //reads file
+	string line;
+	ifstream file(fileName.c_str());
+
+	if(file.is_open()){
+		while(getline(file,line)){
+			CleanLine(RemoveWhitespace(line));
+		}
+		file.close();
+	}else{
+		cout << "Unable to locate '" << fileName << "'" << endl;
+		return false;
+	}
+	return true;
+}
+
+
+
+int main(int argc, char* argv[]){
+	string USAGE = "Usage: ./mb++ [filename]";
+	if(argc != 2){
+		cout << USAGE << endl;
+		return 0;
+	}
+
+	if(!ReadFile(string(argv[1]))){return 0;}
+
+	cout << toAssemble.size() << endl;
 	return 0;
-} 
+}
